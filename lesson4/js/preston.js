c674b1459b2df0f508a025acac630399e4ae391a
elilaiono <elilaiono@gmail.com>
2020-05-16 21:18:49 -0700

    patial addition of css and js

---
+++ lesson4/js/preston.js
@@ -0,0 +1,15 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+let week_names = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thurday' , 'Friday' , 'Saturday'];
+let month_names = ['January' , 'February' , 'March' , 'April' , 'May' , 'June' , 'July' , 'August' , 'September' , 'October' , 
+'November' , 'December'];
+let date = new Date();
+let weekname = week_names[date.getDay()];
+let monthname = month_names[date.getMonth()];
+
+const dateoutput = document.querySelector('.currentdate');
+
+dateoutput.textContent = weekname + " ," + date.getDate() + " " + monthname + " " + date.getFullYear();
