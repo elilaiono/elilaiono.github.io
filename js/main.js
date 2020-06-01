96603a79a4f582b47b03b3061fe3dcc9d52c69e1
elilaiono <elilaiono@gmail.com>
2020-06-01 11:51:21 -0700

    added web fonts javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 document.getElementById("last-mod").innerHTML = document.lastModified;
+
+WebFont.load({
+    google: {
+      families: [
+         'Lato'
+      ]
+    }
+  });
